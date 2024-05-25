220bde359b1073b476efd9aa5c3480d4805b0b67
mariorodri28 <mario.rodri9@gmail.com>
2024-05-25 20:04:34 +0200

    component creation and routes established

--- PreguntaTech/src/app/app.routes.ts
+++ PreguntaTech/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { TestComponent } from './pages/test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'landing', component: LandingComponent },
+];
