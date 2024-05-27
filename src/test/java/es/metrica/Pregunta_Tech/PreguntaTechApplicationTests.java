d0b11a56e6111cc1dcf934b07e349aaf81b39bd6
FRANCISCO LINARES <FRANCISCO LINARES@P0556>
2024-05-27 10:07:08 +0200

    Created SpringBoot project

---
+++ src/test/java/es/metrica/Pregunta_Tech/PreguntaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package es.metrica.Pregunta_Tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
