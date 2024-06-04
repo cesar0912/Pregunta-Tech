1124b58db6e5ea5b0cfbf01e58c15c2db51c09c4
cesar0912 <96296202+cesar0912@users.noreply.github.com>
2024-06-04 11:27:01 +0200

    test

--- src/main/resources/static/src/app/pages/final-exam-view/final-exam-view.component.ts
+++ src/main/resources/static/src/app/pages/final-exam-view/final-exam-view.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-final-exam-view',
   templateUrl: './final-exam-view.component.html',
   styleUrls: ['./final-exam-view.component.css'],
   standalone: true,
+  imports: [],
 })
 export class FinalExamViewComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log("compnente creadp");
+  }
+  navigateLanding() {
+    this.router.navigate(['landing']);
+  }
 }
