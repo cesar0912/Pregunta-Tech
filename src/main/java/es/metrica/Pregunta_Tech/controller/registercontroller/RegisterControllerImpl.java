2dc3e0eb764690b5062861582428135427f9083b
FRANCISCO LINARES <FRANCISCO LINARES@192.168.1.41>
2024-06-04 16:58:43 +0200

    JWT Class created

--- src/main/java/es/metrica/Pregunta_Tech/controller/registercontroller/RegisterControllerImpl.java
+++ src/main/java/es/metrica/Pregunta_Tech/controller/registercontroller/RegisterControllerImpl.java
@@ -1,6 +1,7 @@
 package es.metrica.Pregunta_Tech.controller.registercontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/es/metrica/Pregunta_Tech/utils/jwt/Jwt.java
+++ src/main/java/es/metrica/Pregunta_Tech/utils/jwt/Jwt.java
@@ -1,5 +1,55 @@
 package es.metrica.Pregunta_Tech.utils.jwt;
 
-public class Jwt {
+import java.util.Date;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+import es.metrica.Pregunta_Tech.model.User;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class Jwt {
+	
+	//@Value("${application.security.jwt.secret}")
+	private String KEY = "12fthuijo3xfetryuljhfdsewrtikjhgfdswe4r5ty67uikjhfdsew4";
+	
+	//@Value("${application.security.jwt.hoursTillExp}")
+	private long EXPIRATION_HOURS = 12;
+	/*
+	 * @param user User with at least name and Id
+	 * @param claims Map<String, Object> with a key-value containing the (at least) the password.
+	 * @return Token
+	 */
+	public String generateToken(User user, Map<String, Object> claims) {
+		Date issuedDate = new Date(System.currentTimeMillis());
+		Date expiration = new Date(issuedDate.getTime() + (EXPIRATION_HOURS *3600000));
+		return Jwts.builder()
+						.setClaims(claims)
+						.setSubject(user.getName())
+						.setId(user.getId()+"")
+						.setIssuedAt(issuedDate)
+						.setExpiration(expiration)
+						.setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+						.signWith(SignatureAlgorithm.HS256, KEY)
+						.compact();
+	}
+	/*
+	 * @param jwt complete token
+	 * @return ID from user in the token
+	 */
+	public long getUser(String jwt) {
+
+		String id = Jwts.parser()
+						.setSigningKey(KEY)
+						.parseClaimsJws(jwt)
+						.getBody()
+						.getId();
+		return Long.parseLong(id);
+  
+  }
 }
